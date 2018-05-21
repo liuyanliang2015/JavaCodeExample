c23a409b5a49917bf181a2e1d4e7f0fe03959b9b
liuyanliang <bert2017@126.com>
2018-05-21 15:33:55 +0800

    323423

--- src/com/test/outOfMemory/TestOutOfMemory1.java
+++ src/com/test/outOfMemory/TestOutOfMemory1.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 /**
  * 模拟堆溢出
+ * java.lang.OutOfMemoryError: Java heap space
  * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
  *
  */
 public class TestOutOfMemory1 {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		while(true){
-			list.add("abcdefg");
+		List<byte[]> list = new ArrayList<byte[]>();
+		for(int i = 0; i <1024;i++){
+			list.add(new byte[1024*1024]);
 		}
 	}
 
